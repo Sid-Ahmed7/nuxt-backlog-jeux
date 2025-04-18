cbdad072ad9dbdfdd00fc5e2975f986e38d3fb2b
Sid-Ahmed7 <sido91080@hotmail.fr>
2025-04-18 22:25:22 +0200

    add style

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import BacklogTrack from '@/components/Home/BacklogTrack.vue'
-import UpcomingGames from '@/components/Home/UpcomingGames.vue'
+import UpcomingGames from '@/components/Home/UpComingGames.vue'
 import LastOuting from '@/components/Home/LastOuting.vue'
 import RatingGames from '@/components/Home/RatingGames.vue'
 </script>
@@ -25,5 +25,22 @@ import RatingGames from '@/components/Home/RatingGames.vue'
   margin: 0 auto;
   padding: 2rem 0;
   max-width: 100%;
+}
+
+@media screen and (max-width: 1024px) {
+  .game-recommendations {
+    flex-direction: column;
+    align-items: center;
+    width: 90%;
+    gap: 1.5rem;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .game-recommendations {
+    width: 95%;
+    padding: 1.5rem 0;
+    gap: 1rem;
+  }
 }
 </style>
