c2d4419dcc6b2ed16f2a388bfc016b857dcbd061
Sid-Ahmed7 <sido91080@hotmail.fr>
2025-07-20 21:28:19 +0200

    update

--- pages/edit-profile/[userId].vue
+++ pages/edit-profile/[userId].vue
@@ -7,25 +7,10 @@ const userId = route.params.userId as string;
 </script>
 
 <template>
-    <div class="edit-profile-container">
+  <div class="max-w-3xl mx-auto px-4 py-8">
         <h1>Modifier le profil</h1>
     <ProfileInfo :userId="userId"/>
     
     </div>
 
 </template>
-
-<style scoped>
-
-.edit-profile-container {
-    max-width: 800px;
-    margin: 0 auto;
-    padding: 2rem 1rem;
-}
-h1 {
-    text-align: center;
-    font-size: 2rem;
-    margin-bottom: 2rem;
-    color: var(--main-color);
-}
-</style>
